39b9863739b2f73381c86224baae27cf2cab1610
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-10-04 22:01:31 +0800

    10.4

---
+++ maopao2/src/javadome.java
@@ -0,0 +1,54 @@
+
+import java.util.Arrays;
+public class javadome {
+    public static int[] maopao(int[] array) {
+        int i = array.length - 1;
+        boolean ret=false;
+        int j = 0;
+        for (i = array.length - 1; j < i; i--) {
+           import java.util.Arrays;
+            public class javadome {
+
+                public static int[] maopao(int[] array) {
+                    int i= array.length-1;
+                    int j=0;
+                    for(i=array.length-1;j<i;i--) {
+                        for (j = 0; j < i; j++) {
+                            if (array[j] > array[j + 1]) {
+                                int a=array[j+1];
+                                array[j+1]=array[j];
+                                array[j]=a;
+                            }
+                        }
+                    }
+                }
+        return array;
+            }
+            public static void main(String[] args) {
+                int []array={1,3,9,4,5,7,8};
+                maopao(array);
+                System.out.println(Arrays.toString(maopao(array)));
+            }
+        }
+
+        for (j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int a = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = a;
+                    ret=true;
+                }if(ret==false){
+                    break;
+                }
+            }
+        }
+
+        return array;
+    }
+    public static void main(String[] args) {
+        int []array={1,3,9,4,5,7,8};
+        maopao(array);
+        System.out.println(Arrays.toString(maopao(array)));
+    }
+}
+
