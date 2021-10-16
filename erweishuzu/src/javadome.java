cded67a56a7d29c271f5c97558cc92eb415a9f10
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-10-16 23:19:26 +0800

    10.16

---
+++ erweishuzu/src/javadome.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class javadome {
+    public static void main(String[] args) {
+        int [][] array={{1,2,3,4},{5,6,7,7,9}};
+
+        System.out.println(Arrays.toString(array));
+    }
+}
