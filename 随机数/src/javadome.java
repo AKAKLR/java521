b126983862f8eccafd087cefeb82b32990385837
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-10-21 22:11:02 +0800

    10.21

---
+++ 随机数/src/javadome.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class javadome {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int rand = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("请输入一个数字");
+            int a = scanner.nextInt();
+            if (a < rand) {
+                System.out.println("猜小了");
+
+            } else if (a > rand) {
+                System.out.println("猜大了");
+
+            } else {
+                System.out.println("猜对了");
+                break;
+            }
+        }
+    }
+}
