a92a77846c3a65c80844af7a50387d62a0df8c1c
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-10-23 22:03:09 +0800

    10.23

---
+++ 乘法表/src/javadome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class javadome {
+    public static void main(String[] args) {
+        System.out.println("请输入一个数字");
+        Scanner scanner=new Scanner(System.in);
+        int arr1[] = scanner.nextInt();
+
+            System.out.println("");
+        }
+
+    }
+}
