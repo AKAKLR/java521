678b594383951eb169fac4120e5cf77efd8fb475
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-10-30 21:26:06 +0800

    10.30

---
+++ 10.30zuo/src/javadome.java
@@ -0,0 +1,14 @@
+
+class Test {
+    public  int name=1;
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
+public class javadome {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Test test = new Test();
+        System.out.println(test.name);
+    }
+}
