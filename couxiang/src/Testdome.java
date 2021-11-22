f401a1a5220f64f69b732b58e993d3ba86daf8e2
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-11-22 21:26:16 +0800

    11.22

---
+++ couxiang/src/Testdome.java
@@ -0,0 +1,24 @@
+abstract  class A {
+    int a = 10;
+    String name = "SHIERZGOU";
+
+    public void func() {
+        System.out.println("师二狗");
+    }
+
+    public abstract void draw();
+}
+
+abstract class  B extends A{
+    @Override
+    public  void draw(){
+        System.out.println("SHIERGOU0.0");
+    }
+}
+public class Testdome {
+    public static void main(String[] args) {
+        
+
+
+
+}
