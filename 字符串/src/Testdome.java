d40df505aaea1a28cdf60cdcde9597f6987e1ff4
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-11-26 21:21:56 +0800

    11.26

---
+++ 字符串/src/Testdome.java
@@ -0,0 +1,9 @@
+public class Testdome {
+    public static void main(String[] args) {
+        String name="SBSJY";
+        for (int i=0;i<10;i++){
+            name+=i;
+        }
+        System.out.println(name);
+    }
+}
