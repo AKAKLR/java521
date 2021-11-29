5d40abd54dbbae13a3c9d82f7557a3371437e0f8
aka快乐人 <87457989+AKAKLR@users.noreply.github.com>
2021-11-29 21:47:53 +0800

    11.29

---
+++ 1-nleijia/src/javadome.java
@@ -0,0 +1,11 @@
+public class javadome {
+    public static void main(String[] args) {
+        sum1(100);
+    }
+
+    public static void sum1(int a) {
+        int sum = 0;
+        sum = a*(a + 1) / 2;
+        System.out.println(sum);
+    }
+    }
